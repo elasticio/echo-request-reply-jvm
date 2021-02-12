0a312633cb93b6babcae2cf5f85e6d116ed59e86
Leonid Kuzmin <leonid.kuzmin@elastic.io>
2021-02-12 13:58:37 +0200

    initial commit

---
+++ src/main/java/io/elastic/petstore/triggers/Reply.java
@@ -0,0 +1,49 @@
+package io.elastic.reply.triggers;
+
+import io.elastic.api.ExecutionParameters;
+import io.elastic.api.Function;
+import io.elastic.api.Message;
+import io.elastic.api.HttpReply;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonString;
+
+
+/**
+ * Trigger to get pets by status.
+ */
+public class Reply implements Function {
+    private static final Logger logger = LoggerFactory.getLogger(GetPetsByStatus.class);
+
+    /**
+     * @param parameters execution parameters
+     */
+    @Override
+    public void execute(final ExecutionParameters parameters) {
+        final JsonObject body = Json.createObjectBuilder()
+                .add("echo", parameters.getMessage().getBody())
+                .build();
+
+        final Message data
+                = new Message.Builder().body(body).build();
+
+        final HttpReply httpReply = new HttpReply.Builder()
+                .header("Content-type", "application/json")
+                .status(HttpReply.Status.ACCEPTED)
+                .content(new ByteArrayInputStream(JSON.stringify(body).getBytes()))
+                .build();
+
+        logger.info("Emitting data");
+
+        // emitting the message to the platform
+        parameters.getEventEmitter().emitData(data);
+
+
+        logger.info("Emitting reply");
+        parameters.getEventEmitter().emitHttpReply(httpReply);
+    }
+}
